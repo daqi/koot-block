2bbbe31a6dbffc092a0f0be118b8e448ef11d668
qiyunjiang <qiyunjiang@cmcm.com>
2019-12-13 18:26:55 +0800

    rm .docz

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'src/image-clip/index.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-image-clip-index" */ 'src/image-clip/index.mdx'
-    ),
-}
